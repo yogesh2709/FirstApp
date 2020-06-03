b0712a2d1f5d35fa32dc78d490be65a846dc5d65
Yogesh Vishwanathan <vishwanathan.yogesh17@siesgst.ac.in>
2020-06-04 00:59:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private fun countMe(view: View){}
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            view.findViewById<Button>(R.id.count_button).setOnClickListener {
+                countMe(view)
+
+                    // Get the text view
+                    val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+
+                    // Get the value of the text view.
+                    val countString = showCountTextView.text.toString()
+
+                    // Convert value to a number and increment it
+                    var count = countString.toInt()
+                    count++
+
+                    // Display the new value in the text view.
+                    showCountTextView.text = count.toString()
+                }
+
+
+
+            }
+        }
+    }
+
